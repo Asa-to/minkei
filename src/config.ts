82305e810fb92a5b126a9f8513eb1c64af232c0b
Asa-to <sethuna03@gmail.com>
2023-01-13 13:18:54 +0900

    configを追加

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const configSchema = z.object({
+  HASURA_ENDPOINT: z.string().url(),
+});
+
+type Config = z.infer<typeof configSchema>;
+
+const env = {
+  HASURA_ENDPOINT: process.env.HASURA_ENDPOINT,
+}
+
+export const config = () => {
+  // configの型チェック(足りない型があればcatchされる)
+  try {
+    configSchema.parse(env);
+  } catch (e) {
+    console.error(e);
+  }
+
+  return env as Config;
+}
