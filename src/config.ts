afeb4ab22ec7ab9397835350efc7e93035ada013
Asa-to <sethuna03@gmail.com>
2023-01-17 08:16:59 +0900

    config update

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,14 @@ import { z } from 'zod';
 
 const configSchema = z.object({
   HASURA_ENDPOINT: z.string().url(),
+  HASURA_SECRET_KEY: z.string(),
 });
 
 type Config = z.infer<typeof configSchema>;
 
-const env = {
-  HASURA_ENDPOINT: process.env.HASURA_ENDPOINT,
+const env: Config = {
+  HASURA_ENDPOINT: process.env.NEXT_PUBLIC_HASURA_ENDPOINT as string,
+  HASURA_SECRET_KEY: process.env.NEXT_PUBLIC_HASURA_SECRET_KEY as string,
 }
 
 export const getConfig = () => {
