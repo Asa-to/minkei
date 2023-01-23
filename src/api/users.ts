ceedd7085b66954ab1842d33d4a36ecbdc9a17e0
Asa-to <sethuna03@gmail.com>
2023-01-23 10:18:23 +0900

    user mutation

--- src/api/users.ts
+++ src/api/users.ts
@@ -10,8 +10,19 @@ export const USER = gql`
 `;
 
 export const INSERT_USER = gql`
-  mutation INSERT_USER($name: String!) {
+  mutation InsertUser($name: String!) {
     insert_user(objects: { name: $name }) {
+      returning {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const DELETE_UESR2 = gql`
+  mutation MyMutation($_eq: uuid) {
+    delete_user(where: { id: { _eq: $_eq } }) {
       returning {
         id
         name
