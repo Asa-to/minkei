9936137e1c7029543488a968bab3b691780b72ec
Asa-to <sethuna03@gmail.com>
2023-01-26 19:59:23 +0900

    money record追加用の関数を実装

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,11 @@
 import 'dayjs/locale/ja';
+import { FormData } from 'src/components/molecules/InputModal/inputForm';
 import { LedgerCalendar } from 'src/components/molecules/LedgerCalendar';
-import { useMoneyRecordLazyQuery, useMoneyRecordQuery } from 'src/gql/types';
+import {
+  useInsertMoneyRecordMutation,
+  useMoneyRecordLazyQuery,
+  useMoneyRecordQuery,
+} from 'src/gql/types';
 
 const Home = () => {
   const [
@@ -12,18 +17,37 @@ const Home = () => {
     loading: initMoneyLoading,
     error: initMoneyError,
   } = useMoneyRecordQuery();
-
+  const [
+    insertMoney,
+    { data: insertResult, loading: inserting, error: insertError },
+  ] = useInsertMoneyRecordMutation();
   const moneyRecords =
     moneyData?.money_records ?? initialMoney?.money_records ?? [];
   const moneyFetching = moneyLoading || initMoneyLoading;
 
   // GraphQLエラーをキャッチ
-  if (moneyError || initMoneyError) {
+  if (moneyError || initMoneyError || insertError) {
     return <></>;
   }
-
+  const insertForm = ({ money, title, date }: FormData) => {
+    console.log(date);
+    insertMoney({
+      variables: {
+        date,
+        amount: money,
+        title: title,
+        payer_id: '0be67a03-8839-4828-beb6-adc82816e6f5',
+      },
+    });
+  };
   return (
-    <LedgerCalendar moneyRecords={moneyRecords} moneyFetching={moneyFetching} />
+    <>
+      <LedgerCalendar
+        moneyRecords={moneyRecords}
+        moneyFetching={moneyFetching}
+        onDayClick={insertForm}
+      />
+    </>
   );
 };
 
